c2b66f646e9dc3cc7120f103467b03f1ef39e2a3
Nicholas <nicholas.k.santosa@student.uts.edu.au>
2018-05-24 00:31:58 +1000

    Make script template

--- application/controllers/Script.php
+++ application/controllers/Script.php
@@ -7,18 +7,21 @@ class Script extends CI_Controller {
     {
         $data['title'] = "Script";
         $data['view'] = "script";
-		$this->run();
+		$this->populate_usage();
     }
-	
-	private function run()
+
+	private function populate_usage()
 	{
 		$run = "run";
-		for($i=0;$i>3;$i++) {
-			for($hour=0;$hour>25;$hour++) {
-				
+
+		for($i=0;$i<3;$i++) {
+		    $date = "date";
+		    echo "ADD TO DATE<br>";
+			for($hour=0;$hour<25;$hour++) {
+                echo "ADD USAGE<br>";
 			}
 		}
 		//print_r($this->db->query("SELECT * FROM users")->result_array());
-		echo "done";
+		echo "DONE";
 	}
 }
