47edd3d01f302e58f62db64246227fb00085ca95
Nicholas <nicholas.k.santosa@student.uts.edu.au>
2018-05-24 16:00:17 +1000

    Finished populate usage and production script

--- application/controllers/Script.php
+++ application/controllers/Script.php
@@ -5,23 +5,49 @@ class Script extends CI_Controller {
 
     public function index()
     {
-        $data['title'] = "Script";
-        $data['view'] = "script";
-		$this->populate_usage();
+		//$this->populate_usage();
+        //$this->populate_production();
+        echo "No script run.";
     }
 
 	private function populate_usage()
 	{
-		$run = "run";
-
 		for($i=0;$i<3;$i++) {
-		    $date = "date";
-		    echo "ADD TO DATE<br>";
-			for($hour=0;$hour<25;$hour++) {
-                echo "ADD USAGE<br>";
+		    $d = mktime(0, 0, 0, 5, 14+$i, 2018);
+		    $date = date('Y-m-d', $d);
+		    echo "ADD TO DATE $date<br>";
+			for($hour=0;$hour<24;$hour++) {
+                echo "ADD USAGE TIME $hour<br>";
+                $this->db->insert('node_usages', array('nodeid'=>'1', 'date'=>$date, 'time'=>$hour, 'amount'=>0.25));
+                $this->db->insert('node_usages', array('nodeid'=>'2', 'date'=>$date, 'time'=>$hour, 'amount'=>0.15));
+                $this->db->insert('node_usages', array('nodeid'=>'3', 'date'=>$date, 'time'=>$hour, 'amount'=>0.014));
+                $this->db->insert('node_usages', array('nodeid'=>'4', 'date'=>$date, 'time'=>$hour, 'amount'=>0.034));
+                $this->db->insert('node_usages', array('nodeid'=>'5', 'date'=>$date, 'time'=>$hour, 'amount'=>0.243));
+                $this->db->insert('node_usages', array('nodeid'=>'6', 'date'=>$date, 'time'=>$hour, 'amount'=>0.14));
 			}
 		}
 		//print_r($this->db->query("SELECT * FROM users")->result_array());
 		echo "DONE";
 	}
+
+	private function populate_production()
+    {
+        $base = 2.208;
+        for($i=0;$i<3;$i++) {
+            $d = mktime(0, 0, 0, 5, 14+$i, 2018);
+            $date = date('Y-m-d', $d);
+            echo "ADD TO DATE $date<br>";
+            for($hour=0;$hour<24;$hour++) {
+                echo "ADD PRODUCTION TIME $hour<br>";
+
+                if(rand(1,100) <= 50) {
+                    $final = $base - ($base*rand(1,40)/100);
+                } else {
+                    $final = $base;
+                }
+                $this->db->insert('solar_productions', array('solarid'=>'1', 'date'=>$date, 'time'=>$hour, 'amount'=>$final));
+            }
+        }
+        echo "DONE";
+    }
 }
