87bb2b7268e73b2541c1a9bb286d64c4c77bd3c7
Nicholas <nicholas.k.santosa@student.uts.edu.au>
2018-05-30 13:56:26 +1000

    Add electric vehicle chart in admin page

--- application/views/admin.php
+++ application/views/admin.php
@@ -19,6 +19,9 @@
             <li class="nav-item">
                 <a class="nav-link" data-toggle="tab" href="#buydischarge">Buy & Discharge Chart</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#electricvehicle">Electric Vehicle Chart</a>
+            </li>
         </ul>
     </div>
 </div>
@@ -49,6 +52,21 @@
                 <i class="fa fa-area-chart"></i> Buy & Discharge</div>
             <div class="card-body">
                 <canvas id="buydischargeChart" width="100%" height="30"></canvas>
+            </div>
+            <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+        </div>
+    </div>
+    <div class="tab-pane fade" id="electricvehicle">
+        <div class="row">
+            <div class="col-12">
+                <h2>Electric Vehicle Charge & Discharge Chart</h2>
+            </div>
+        </div>
+        <div class="card mb-3">
+            <div class="card-header">
+                <i class="fa fa-area-chart"></i> Electric Vehicle Charge & Discharge</div>
+            <div class="card-body">
+                <canvas id="electricvehicleChart" width="100%" height="30"></canvas>
             </div>
             <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
         </div>
@@ -110,6 +128,56 @@
     });
 
     var ctx = document.getElementById("buydischargeChart");
+    var myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10"],
+            datasets: [{
+                label: "Sessions",
+                lineTension: 0.3,
+                backgroundColor: "rgba(2,117,216,0.2)",
+                borderColor: "rgba(2,117,216,1)",
+                pointRadius: 5,
+                pointBackgroundColor: "rgba(2,117,216,1)",
+                pointBorderColor: "rgba(255,255,255,0.8)",
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(2,117,216,1)",
+                pointHitRadius: 20,
+                pointBorderWidth: 2,
+                data: [10000, 30162, 26263, 18287, 28682, 33259, 25849 , 32651, 24159, 38451],
+            }],
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    time: {
+                        unit: 'date'
+                    },
+                    gridLines: {
+                        display: false
+                    },
+                    ticks: {
+                        maxTicksLimit: 7
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        min: 0,
+                        max: 40000,
+                        maxTicksLimit: 5
+                    },
+                    gridLines: {
+                        color: "rgba(0, 0, 0, .125)",
+                    }
+                }],
+            },
+            legend: {
+                display: false
+            }
+        }
+    });
+
+    var ctx = document.getElementById("electricvehicleChart");
     var myLineChart = new Chart(ctx, {
         type: 'line',
         data: {
